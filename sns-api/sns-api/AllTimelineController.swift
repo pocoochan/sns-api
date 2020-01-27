72c46dd6e5c61aaf65df52a16bc46945a679f235
Saya <sayamatsui327@gmal.com>
2020-01-27 15:25:46 +0900

    ①userの中のnameだけ取りたい問題。②TableViewにデータが反映されない問題

--- sns-api/sns-api/AllTimelineController.swift
+++ sns-api/sns-api/AllTimelineController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class AllTimelineController: UIViewController {
+class AllTimelineController: UITableViewController {
     
-
-    @IBAction func timeline(_ sender: Any) {
+    var response: [[String: Any]]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*
+         すべてのタイムラインを取得するAPI
+         */
         let config: URLSessionConfiguration = URLSessionConfiguration.default
         
         let session: URLSession = URLSession(configuration: config)
         
-        //テキストフィールドに入力されたStringと取得して変数にいれる
-//        let page = searchPage.text
-//        let limit = searchLimit.text
-//        let query = searchQuery.text
+        
+        //テキストフィールドに入力[]されたStringと取得して変数にいれる
+        //        let page = searchPage.text
+        //        let limit = searchLimit.text
+        //        let query = searchQuery.text
         
         //URLを組み立てている
         var urlComponents = URLComponents()
@@ -27,18 +34,18 @@ class AllTimelineController: UIViewController {
         urlComponents.host = "teachapi.herokuapp.com"
         urlComponents.path = "/posts"
         urlComponents.queryItems = [
-//            URLQueryItem(name: "page", value: page),
-//            URLQueryItem(name: "limit", value: limit),
-//            URLQueryItem(name: "query", value: query)
+            //            URLQueryItem(name: "page", value: page),
+            //            URLQueryItem(name: "limit", value: limit),
+            //            URLQueryItem(name: "query", value: query)
         ]
         
         let url: URL = urlComponents.url!
         var req: URLRequest = URLRequest(url: url)
         req.httpMethod = "GET"
-
+        
         
         print(url)
-            
+        
         //ヘッダーを付与
         let defaults = UserDefaults.standard
         let myToken = defaults.string(forKey: "responseToken")!
@@ -53,17 +60,81 @@ class AllTimelineController: UIViewController {
                 let response: [[String: Any]] = try JSONSerialization.jsonObject(with: data!, options: []) as! [[String: Any]]
                 
                 print(response)
-
-                
+                self.response = response
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             } catch{
                 
             }
             
         }
         task.resume()
+        
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    /*
+     APIで取得したデータをテーブルビューに表示させるよ
+     */
+    var userPhotos = ["1","2","defaultIcon1","defaultIcon2"]
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+//        let image:UIImage = UIImage(named:"defaultIcon")!
+//        let resizeImage = image.resized(toWidth: 50)
+        
+        
+        let cellIdentifier: String = "AllTimelineCustomCell"
+        if let myCell: AllTimelineCustomCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? AllTimelineCustomCell {
+//            myCell.userProfileIcon?.image = UIImage(named: self.userPhotos[].randomElement()!)
+            myCell.userProfileIcon?.image = UIImage(named: "defaultIcon")
+//            myCell.userProfileName?.text = response?[indexPath.row](["user"] as! [String:Any])["name"]
+            myCell.postDate?.text = response?[indexPath.row]["created_at"] as? String
+            myCell.userProfileIcon.layer.cornerRadius = 50
+            return myCell
+            
+        }
+        
+        let myCell = AllTimelineCustomCell(style: .default, reuseIdentifier: "AllTimelineCustomCell")
+//        myCell.userProfileIcon?.image = resizeImage
+        myCell.userProfileIcon?.image = UIImage(named: "defaultIcon")
+        myCell.userProfileName?.text = response?[indexPath.row]["text"] as? String
+        myCell.postDate?.text = response?[indexPath.row]["created_at"] as? String
+         myCell.userProfileIcon.layer.cornerRadius = 50
+        return myCell
     }
+    
+    //生成するセルの個数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let response = response {
+            return response.count
+        }
+        return 0
+    }
+    
+    //1つ1つのセルの高さ
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(107)
+    }
+    
 }
+
+
+class AllTimelineCustomCell: UITableViewCell {
+    @IBOutlet weak var userProfileIcon: UIImageView!
+    @IBOutlet weak var userProfileName: UILabel!
+    @IBOutlet weak var postDate: UILabel!
+}
+
+
+extension UIImage {
+    func resized(toWidth width: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+   
+}
+
--- sns-api/sns-api/MyPageController.swift
+++ sns-api/sns-api/MyPageController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class MyPageController: UIViewController {
+class MyPageController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var response: [[String: Any]]?
     
     @IBAction func gotoUserEditViewController(_ sender: Any) {
         DispatchQueue.main.async {
@@ -26,17 +28,7 @@ class MyPageController: UIViewController {
             print("ツイート作成画面に遷移成功してるよ")
         }
     }
-    
-    @IBAction func SearchPage(_ sender: Any) {
-        DispatchQueue.main.async {
-            let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let SearchController = storyboard.instantiateViewController(withIdentifier: "SearchController")
-            self.present(SearchController, animated: true, completion: nil)
-            print("ユーザー検索ページに遷移成功してるよ")
-        }
         
-    }
-    
     @IBAction func gotoTimeline(_ sender: Any) {
         DispatchQueue.main.async {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -47,13 +39,136 @@ class MyPageController: UIViewController {
         }
     }
     @IBOutlet weak var myName: UITextView!
-//    let defaults = UserDefaults.standard
-//    myName.text = defaults.string(forKey: "responseName")
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var myBio: UITextView!
+    
+    
+    //    画面に遷移するたびに読み込まれる
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let config: URLSessionConfiguration = URLSessionConfiguration.default
+        
+        let session: URLSession = URLSession(configuration: config)
+        
+        //テキストフィールドに入力されたStringと取得して変数にいれる
+        //        let page = searchPage.text
+        //        let limit = searchLimit.text
+        //        let query = searchQuery.text
+        
+        //URLを組み立てている
+        let defaults = UserDefaults.standard
+        let myId = defaults.string(forKey: "responseId")!
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = "teachapi.herokuapp.com"
+        urlComponents.path = "/users/\(myId)/timeline"
+        urlComponents.queryItems = [
+            //            URLQueryItem(name: "page", value: page),
+            //            URLQueryItem(name: "limit", value: limit),
+            //            URLQueryItem(name: "query", value: query)
+        ]
+        
+        let url: URL = urlComponents.url!
+        var req: URLRequest = URLRequest(url: url)
+        req.httpMethod = "GET"
+        
+        
+        print(url)
+        
+        //ヘッダーを付与
+        let myToken = defaults.string(forKey: "responseToken")!
+        req.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        req.setValue("Bearer " + myToken, forHTTPHeaderField: "Authorization")
+        
+        
+        //APIを呼ぶよ
+        let task = session.dataTask(with: req){(data, response, error) in
+            
+            do {
+                let response: [[String: Any]] = try JSONSerialization.jsonObject(with: data!, options: []) as! [[String: Any]]
+                
+                print(response)
+                self.response = response
+                //                DispatchQueue.main.async {
+                //                    self.tableView.reloadData()
+                //                }
+                //
+            } catch{
+                
+            }
+            
+        }
+        task.resume()
     }
     
- 
+
+    @IBOutlet weak var myTV: UITableView!
+    
+//カスタムセルの作成
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+    let cellIdentifier: String = "myPageCustomCell"
+    if let myCell: myPageCustomCell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? myPageCustomCell {
+
+        myCell.userIcon?.image = UIImage(named: "defaultIcon")
+        myCell.myUserName?.text = response?[indexPath.row](["user"] as! [String:Any])["name"]
+        myCell.myPost?.text = response?[indexPath.row]["text"] as? String
+        myCell.postDate?.text = response?[indexPath.row]["created_at"] as? String
+        myCell.userIcon.layer.cornerRadius = 50
+        return myCell
+    }
+    
+    let myCell = myPageCustomCell(style: .default, reuseIdentifier: "myPageCustomCell")
+        myCell.userIcon?.image = UIImage(named: "defaultIcon")
+        myCell.myUserName?.text = response?[indexPath.row](["user"] as! [String:Any])["name"]
+        myCell.myPost?.text = response?[indexPath.row]["text"] as? String
+        myCell.postDate?.text = response?[indexPath.row]["created_at"] as? String
+        myCell.userIcon.layer.cornerRadius = 50
+        return myCell
+    }
+    
+    //生成するセルの個数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let response = response {
+            return response.count
+        }
+        return 0
+    }
+    
+    //1つ1つのセルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(107)
+    }
+    
+    
+    override func viewDidLoad() {
+        
+        myTV.delegate = self
+        myTV.dataSource = self
+       
+        
+        super.viewDidLoad()
+        
+        //マイページに名前と自己紹介を表示させる
+        let defaults = UserDefaults.standard
+        myName.text = defaults.string(forKey: "responseName")
+        myBio.text = defaults.string(forKey: "responseBio")
+    }
+    
+
+    //
+    //    //画面をはなれてるときに読み込まれる
+    //    override func viewWillDisappear(_ animated: Bool) {
+    //        super.viewWillDisappear(animated)
+    //
+    //    }
+
+}
+
+class myPageCustomCell: UITableViewCell{
+    @IBOutlet weak var userIcon: UIImageView!
+    @IBOutlet weak var myUserName: UILabel!
+    @IBOutlet weak var myPost: UILabel!
+    @IBOutlet weak var postDate: UILabel!
+    @IBOutlet weak var postEdit: UIButton!
 }
