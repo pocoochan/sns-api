7fda11533ce894178789eaf26b7b2451e599114c
Saya <sayamatsui327@gmal.com>
2020-01-28 19:06:39 +0900

    NavigationBarViewController.swiftを作成した

---
+++ sns-api/sns-api/NavigationBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationBarViewController.swift
+//  sns-api
+//
+//  Created by saya on 2020/01/28.
+//  Copyright © 2020 saya. All rights reserved.
+//
+
+import UIKit
+
+class NavigationBarViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
